5a876a86647d6b9b5dcb2600e7f2f9a140621d72
Tony Cihak <tony.cihak@phantom.us>
2019-10-03 11:44:31 -0700

    PAPP-8603 added copyright notice

--- f5bigipltm_consts.py
+++ f5bigipltm_consts.py
@@ -1,2 +1,6 @@
-
-# Define your constants here
+# File: f5bigipltm_consts.py
+#
+# Copyright (c) 2019 Splunk Inc.
+#
+# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
+# without a valid written license from Splunk Inc. is PROHIBITED.
